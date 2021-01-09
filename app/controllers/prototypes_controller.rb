60e2bdc9461d1c498e02670b1ef1ff93c647fe1f
takeshivvv <takeshi082608260826@gmail.com>
2021-01-09 17:09:39 +0900

    プロトタイプ情報の投稿機能を実装しよう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,14 +1,41 @@
 class PrototypesController < ApplicationController
-
+  before_action :set_prototype, only: [:edit, :show]
 
   def index
+    @prototypes = Prototype.all
 end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    Prototype.create(prototypes_params)
+  end
+
+  def update
+    if current_user.update(prototypes_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def show
+  end
 
 
 private
   def prototypes_params
-    params.require(:users).permit(:name, :email ).merge(user_id: current_user.id)
+    params.require(:prototype).permit(:title, :concept, :catch_copy, :image ).merge(user_id: current_user.id)
   end 
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
   validates :occupation, presence: true
   validates :position, presence: true
   
-  
+  has_many :prototype
 
   end
